8ee4e58698009b2f091ffb3f322db9d103ec6430
onlineteambitcode <onlineteambitcode@gmail.com>
2025-04-22 22:03:52 +0530

    buid files base URL change

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiBaseUrl: 'http://api.lawyercloud.work.gd',  // Replace with your production API base URL
+    apiBaseUrl: 'http://lawyercloud.work.gd/api',  // Replace with your production API base URL
     jwtExpiresIn: 10 * 60 * 60 * 1000 // 10 hours in milliseconds
   };
 
